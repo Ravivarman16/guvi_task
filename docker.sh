ed15aad5c7b1a612613ecb867096f8d7a2cebdc3
Ravivarman16 <129171351+Ravivarman16@users.noreply.github.com>
2023-10-04 08:43:47 +0530

    shell file to install docker

---
+++ docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#updating the os:
+apt-get update
+
+#installing docker:
+apt-get install -y docker.io
+
+#enable docker service:
+systemctl enable docker
+
+#starting the docker service:
+systemctl start docker
+
+#checking the docker version:
+docker -v
